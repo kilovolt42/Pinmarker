a3bad0d9f141582f6d9cae6d9fc208491d0d1b70
kilovolt42 <kvstevns@gmail.com>
2013-12-16 15:25:37 -0500

    Add new pin screen

---
+++ Pinmark/PMNewPinTVC.h
@@ -0,0 +1,13 @@
+//
+//  PMNewPinTVC.h
+//  Pinmark
+//
+//  Created by Kyle Stevens on 12/16/13.
+//  Copyright (c) 2013 kilovolt42. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PMNewPinTVC : UITableViewController
+
+@end
---
+++ Pinmark/PMNewPinTVC.m
@@ -0,0 +1,65 @@
+//
+//  PMNewPinTVC.m
+//  Pinmark
+//
+//  Created by Kyle Stevens on 12/16/13.
+//  Copyright (c) 2013 kilovolt42. All rights reserved.
+//
+
+#import "PMNewPinTVC.h"
+
+@interface PMNewPinTVC () <UITextFieldDelegate, UITextViewDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *URLTextField;
+@property (weak, nonatomic) IBOutlet UITextField *titleTextField;
+@property (weak, nonatomic) IBOutlet UITextField *tagsTextField;
+@property (weak, nonatomic) IBOutlet UITextView *descriptionTextView;
+@property (weak, nonatomic) IBOutlet UISwitch *readLaterSwitch;
+@property (weak, nonatomic) IBOutlet UISwitch *privateSwitch;
+@property (weak, nonatomic) id activeField;
+@end
+
+@implementation PMNewPinTVC
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	self.URLTextField.delegate = self;
+	self.titleTextField.delegate = self;
+	self.tagsTextField.delegate = self;
+	self.descriptionTextView.delegate = self;
+	
+	UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dismissKeyboard)];
+	[self.view addGestureRecognizer:tap];
+}
+
+- (IBAction)pin:(UIBarButtonItem *)sender {
+
+}
+
+- (void)dismissKeyboard {
+	[self.activeField resignFirstResponder];
+}
+
+- (BOOL)tableView:(UITableView *)tableView shouldHighlightRowAtIndexPath:(NSIndexPath *)indexPath {
+	return NO;
+}
+
+#pragma mark - UITextFieldDelegate
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField {
+	self.activeField = textField;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+	if (textField == self.URLTextField) [self.titleTextField becomeFirstResponder];
+	else if (textField == self.titleTextField) [self.tagsTextField becomeFirstResponder];
+	else if (textField == self.tagsTextField) [self.descriptionTextView becomeFirstResponder];
+	return NO;
+}
+
+#pragma mark - UITextViewDelegate
+
+- (void)textViewDidBeginEditing:(UITextView *)textView {
+	self.activeField = textView;
+}
+
+@end
