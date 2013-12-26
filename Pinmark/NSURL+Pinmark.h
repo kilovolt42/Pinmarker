d484ae979059e596c9fb1d4fb02008f86b0e1bd6
kilovolt42 <kvstevns@gmail.com>
2013-12-26 13:35:21 -0500

    Add documentation for NSURL category

--- Pinmark/NSURL+Pinmark.h
+++ Pinmark/NSURL+Pinmark.h
@@ -10,6 +10,12 @@
 
 @interface NSURL (Pinmark)
 
+/**
+ * Creates a dictionary of the query parameters in which fields are keys for the corresponding values.
+ * Does not support array parameters at this time.
+ *
+ * @return A dictionary of query parameters.
+ */
 - (NSDictionary *)queryParameters;
 
 @end
