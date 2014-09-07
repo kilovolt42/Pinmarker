4704d99740c26bd2ca347b79492d45f80a9e4099
kilovolt42 <kvstevns@gmail.com>
2014-09-07 18:10:49 -0400

    Remove 'Hide' button from keyboard accessory

--- Pinmark/PMInputAccessoryView.h
+++ Pinmark/PMInputAccessoryView.h
@@ -6,15 +6,8 @@
 //  Copyright (c) 2014 kilovolt42. All rights reserved.
 //
 
-/**
- * The hide button and collection view are intended to mutually
- * exclusively visible. Setting the hidden property of the collection
- * view will automatically set the hide button's hidden property to
- * the inverse. The collection view is hidden by default.
- */
 @interface PMInputAccessoryView : UIInputView
 
-@property (nonatomic, weak) IBOutlet UIButton *hideButton;
 @property (nonatomic, weak) IBOutlet UICollectionView *collectionView;
 
 @end
