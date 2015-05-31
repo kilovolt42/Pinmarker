b5821057817dcd706c6b77f8629d356e1ec03f9d
kilovolt42 <kvstevns@gmail.com>
2015-05-31 16:31:11 -0400

    Configure a Swift bridging header and extend UIView with an IB designable accessibility ID

---
+++ Pinmark/UIView+Inspectables.swift
@@ -0,0 +1,14 @@
+//
+//  UIView+Inspectables.swift
+//  Pinmarker
+//
+//  Created by Kyle Stevens on 5/31/15.
+//  Copyright (c) 2015 kilovolt42. All rights reserved.
+//
+
+extension UIView {
+	@IBInspectable var accessibilityID: String {
+		get { return accessibilityIdentifier }
+		set { accessibilityIdentifier = newValue }
+	}
+}
