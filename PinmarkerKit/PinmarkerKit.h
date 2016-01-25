9b98ca419ad6ff124b2e98ff3de5d2fac003c714
kilovolt42 <kvstevns@gmail.com>
2016-01-24 22:21:11 -0500

    Move the model and a category into PinmarkerKit

--- PinmarkerKit/PinmarkerKit.h
+++ PinmarkerKit/PinmarkerKit.h
@@ -15,6 +15,9 @@ FOUNDATION_EXPORT double PinmarkerKitVersionNumber;
 FOUNDATION_EXPORT const unsigned char PinmarkerKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <PinmarkerKit/PublicHeader.h>
+
 #import <PinmarkerKit/Constants.h>
 #import <PinmarkerKit/PMPinboardService.h>
 #import <PinmarkerKit/NSURL+Pinmark.h>
+#import <PinmarkerKit/NSString+Pinmark.h>
+#import <PinmarkerKit/PMBookmark.h>
